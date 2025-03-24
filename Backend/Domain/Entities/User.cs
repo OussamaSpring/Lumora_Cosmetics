629476e1d3f6ed21bf8a721eba00af9912a84b08
Oussama Azzouz <aoussama.dev@gmail.com>
2025-03-24 22:00:12 +0100

    Add backend structure folder

---
+++ Backend/Domain/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Domain.Entities
+{
+    public class AccountStatus
+    {
+        public short Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class UserRole
+    {
+        public short Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class User
+    {
+        public Guid Id { get; set; }
+        public Guid PersonId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public DateTime? CloseDate { get; set; }
+        public short AccountStatus { get; set; }
+        public short RoleId { get; set; }
+    }
+}
+
+
